9008d837919986e656d900964fe5324d75416b02
Fumio SAGAWA <fumio.sagawa@gmail.com>
2017-09-13 13:06:20 +0900

    define a router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', redirectTo: 'items', pathMatch: 'full' },
+  { path: 'items', loadChildren: './items/items.module#ItemsModule' },
+  { path: 'carts', loadChildren: './carts/carts.module#CartsModule' },
+  { path: 'history', loadChildren: './history/history.module#HistoryModule' },
+  { path: 'user', loadChildren: './user/user.module#UserModule' }
 ];
 
 @NgModule({
