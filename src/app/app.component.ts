04c8d76c37c908b7a12f878b90c9c692223381c7
Fumio SAGAWA <fumio.sagawa@gmail.com>
2017-09-13 13:09:12 +0900

    #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { Component } from '@angular/core';
   selector: 'bso-root',
   template: `
     <h1>
-      Welcome to {{title}}!
+      <a routerLink="">Book Store Online</a>
     </h1>
     <a routerLink="items">商品一覧</a>
     <a routerLink="carts">ショッピングカート一覧</a>
     <a routerLink="history">購入履歴</a>
     <a routerLink="user">購入者情報</a>
+    <a href="https://github.com/albatrosary/BookStoreOnline/">github</a>
     <router-outlet></router-outlet> 
   `,
   styles: []
