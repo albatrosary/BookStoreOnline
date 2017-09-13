1b5e8eae04173fe6f8ec4054cf9721a88d431590
Fumio SAGAWA <fumio.sagawa@gmail.com>
2017-09-13 13:51:11 +0900

    add http://meyerweb.com/eric/tools/css/reset/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bso-root',
   template: `
-    <h1>
-      <a routerLink="">Book Store Online</a>
-    </h1>
-    <a routerLink="items">商品一覧</a>
-    <a routerLink="carts">ショッピングカート一覧</a>
-    <a routerLink="history">購入履歴</a>
-    <a routerLink="user">購入者情報</a>
-    <a href="https://github.com/albatrosary/BookStoreOnline/">github</a>
+    <nav>
+      <h1>
+        <a routerLink="">Book Store Online</a>
+      </h1>
+      <a routerLink="items">商品一覧</a>
+      <a routerLink="carts">ショッピングカート一覧</a>
+      <a routerLink="history">購入履歴</a>
+      <a routerLink="user">購入者情報</a>
+      <a href="https://github.com/albatrosary/BookStoreOnline/">github</a>
+    </nav>
     <router-outlet></router-outlet> 
   `,
   styles: []
